5145022ad04841be1bdc477f75c99b7a070aa9b7
mayongfei <919700733@qq.com>
2018-01-28 22:44:31 +0800

    index page.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,24 +1,4 @@
 
 window.onresize = function (e) {
-    //alert(123);
-    if(window.innerWidth > 1210){
-        document.querySelectorAll("#nav-title ul li a.nav-icon").forEach(function (node, i, list) {
-            node.style.marginLeft = "23px";
-        });
-        document.querySelectorAll("#nav-title ul li a.nav-link").forEach(function (node, i, list) {
-            node.style.marginLeft = "41px";
-        });
-        $("#nav-title ul li a.nav-link").css("margin-left","23px");
-        $("#nav-title ul li a.nav-icon").css("margin-left","41px");
-    }else if(window.innerWidth <= 1210){
-        document.querySelectorAll("#nav-title ul li a.nav-icon").forEach(function (node, i, list) {
-            node.style.marginLeft = "3px";
-        });
-        document.querySelectorAll("#nav-title ul li a.nav-link").forEach(function (node, i, list) {
-            node.style.marginLeft = "3px";
-        });
-        $("#nav-title ul li a.nav-link").css("margin-left","3px");
-        $("#nav-title ul li a.nav-icon").css("margin-left","3px");
-    }
 
 };
