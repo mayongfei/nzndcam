23fc19cd2a72a4c1c02d6a4075b83859b2d5edf3
mayongfei <919700733@qq.com>
2018-03-25 18:20:44 +0800

    uodate port.

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '47.254.44.124', // can be overwritten by process.env.HOST
+    host: '172.20.110.125', // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
